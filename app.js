35e550bd2831bbb0571bc05136c6198a5dacd317
Hasnainqari <100525567+Hasnainqari@users.noreply.github.com>
2022-05-19 12:01:37 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,267 @@
+// // Chapter NO #03 Start;
+
+// // Question No #01;
+// var age = "I am 19 years old";
+// alert(age);
+
+// // Question No #02;
+// var visiting = "You have visited this site 10 times";
+// alert(visiting);
+
+// // Question No #03;
+// var year = "My birth year is 2002.";
+// document.write(year);
+// var year = "Data type of my declared variable is number.";
+// document.write("<br>" + year + "</br>");
+
+// // Question No #04;
+// var visitorsName = "John Doe";
+// document.write("<b>" + visitorsName + "<b>");
+// var Order = "  ordered";
+// document.write(Order);
+// var productTitle = "  5T-shirt(s)";
+// document.write(productTitle);
+// var quantity = "  on XYZ clothing store";
+// document.write(quantity);
+
+// Chapter No #03 Complete;
+
+// Chapter No #04 Start;
+// Question No #01
+// var heading = "Rules for naming JS variables";
+// document.write("<h2>" + heading + "<h2>");
+
+// var legalName = "Variable names can only contain, Numbers, $ and _.For example $my_1stVariable";
+// document.write( "<br>"  + legalName + "<br>");
+
+// var name = "Variables must begin with a letters, $ or _. For example $name, _name or name";
+// document.write(name  );
+
+// var sentitive = "Variable names are case Sensitive";
+// document.write("<br>"  + sentitive + "<br>");
+
+// var illegalName = "Variable names should not be JS keyword";
+// document.write(illegalName);
+
+// Chapter No #04 Complete;
+
+// Chapter No #05 Start;
+// Question No #01;
+// var num1 = 3;
+// var num2 = 5;
+// var result = (num1+num2);
+// document.write("<br>" + result + "<br>");
+
+// // Question No #02;
+// var num3 = 5;
+// var num4 = 3;
+// var result2 = (num3-num4);
+// document.write("<br>" + result2 + "<br>");
+
+// var num5 = 10;
+// var num6 = 2;
+// var result3 = (num4*num5);
+// document.write("<br>" + result3 + "<br>");
+
+// var num7 = 50;
+// var num8 = 5;
+// var result4 = (num7/num8);
+// document.write("<br>" + result4 + "<br>");
+
+// Question No #03;
+// var firstNum = 5;
+// var secondNum = 1;
+// var thirdNum = 7;
+// var fourthNum = 1;
+// var fifthNum = 3;
+// document.write(firstNum + secondNum + thirdNum - fourthNum / fifthNum);
+
+// Question No #04;
+// var ticketPrice = 500;
+// var costParagraph ="Total cost to buy 10 tickets to a movie is";
+// document.write( "<p>" + costParagraph + "</p>" + ticketPrice*10);
+
+// Question No #05;
+// var tableHeading = "TABLE OF 10";
+// document.write("<h1>" + tableHeading + "</h1>");
+// var table = 10;
+// document.write("<br>" + table*1 + "</br>");
+// var table = 10;
+// document.write("<br>" + table*2 + "</br>");
+// var table = 10;
+// document.write("<br>" + table*3 + "</br>");
+// var table = 10;
+// document.write("<br>" + table*4 + "</br>");
+// var table = 10;
+// document.write("<br>" + table*5 + "</br>");
+// var table = 10;
+// document.write("<br>" + table*6 + "</br>");
+// var table = 10;
+// document.write("<br>" + table*7 + "</br>");
+// var table = 10;
+// document.write("<br>" + table*8 + "</br>");
+// var table = 10;
+// document.write("<br>" + table*9 + "</br>");
+// var table = 10;
+// document.write("<br>" + table*10 + "</br>");
+
+// Question No #06;
+// I can't Understand;
+
+// Question No #07;
+// var shoppingHeading = "Shopping Cart";
+// document.write("<h1>" + shoppingHeading + "</h1>");
+
+// var priceOfItem1Text="Price of item 1 is ";
+// var priceOfItem1="650";
+// document.write(priceOfItem1Text+priceOfItem1);
+
+// var quantityOfItem1Text="Quantity of item 1 is ";
+// var quantityOfItem1="3";
+// document.write("<br>" + quantityOfItem1Text+quantityOfItem1 + "</br>");
+
+// var priceOfItem2Text="Price of item 2 is ";
+// var priceOfItem2="100";
+// document.write(priceOfItem2Text+priceOfItem2);
+
+// var quantityOfItem2Text="Quantity of item 2 is ";
+// var quantityOfItem2="7";
+// document.write("<br>" + quantityOfItem2Text+quantityOfItem2 + "</br>");
+
+// var shippingChargesText="Shipping Charges";
+// var shippingCharges="100";
+// document.write(shippingChargesText+shippingCharges);
+
+
+
+// var resultParagraph = "Total cost of your Order is ";
+// var calculate = 650;
+// var calculate1 = 3;
+// var calculate2 = 100;
+// var calculate3 = 7;
+// var shippingCharges = 100;
+// document.write("<br>"+"<br>" + resultParagraph + ((calculate*calculate1) + (calculate2*calculate3) + shippingCharges) + "<br>"+"</br>");
+
+// Question No #08
+// var marksheetHeading = "MARKSHEET";
+// document.write("<h1>" + marksheetHeading + "</h1>");
+// var totalMarksText = "Total Marks: 980";
+// document.write(totalMarksText)
+// var obtainedMarksText = "Obtained Marks: 804";
+// document.write("<br>" + obtainedMarksText + "</br>")
+// var percentage ="Percentage: "
+// var totalMarks = 980;
+// var obtainedMarks = 804;
+// var hundered = 100;
+// document.write(percentage+(obtainedMarks/totalMarks*hundered));
+
+// Question No #09;
+// var currency ="Currency In PKR";
+// document.write("<h1>" + currency + "</h1>");
+// var totalCurrency = "Total Currency in PKR: ";
+// document.write(totalCurrency);
+// var usDollar = 10;
+// var sauidiRyall = 25;
+// document.write((usDollar*104.80)+(sauidiRyall*28));
+
+// Question No #10;
+// var add = 5;
+// var multiply = 10;
+// document.write((add*multiply)/2);
+
+// Question No #11;
+// var ageCalculater ="Age Calculater";
+// document.write("<h1>" + ageCalculater + "</h1>");
+// var currentYearText = "Current Year: 2022";
+// var currentYear = 2022;
+// document.write(currentYearText);
+// var birthYearText = "Birth Year: 2002";
+// var birthYear = 2002;
+// document.write("<br>" + birthYearText + "</br>");
+// var currentAge = "Your Age is: ";
+// document.write((currentAge)+(currentYear-birthYear));
+
+// Question No #12;
+// I can't Understand;
+
+// Question No #13;
+// var headingLifeTime = " The Life Time Supply Calculator";
+// document.write("<h1>" + headingLifeTime + "</h1>");
+
+// var favouriteSnakeText = " Favourite snake: Choclate Chip";
+// document.write( favouriteSnakeText  );
+// var ageCurrentText = "Current Age: ";
+// var ageCurrent = "15";
+// document.write("<br>" + ageCurrentText+ageCurrent + "</br>")
+// var estimatedText = "Estimated Maximum Age: ";
+// var estimated ="65";
+// document.write( estimatedText+estimated)
+// document.write()
+// var amountSnacksText = "Amount of Snacks per day: ";
+// var amountSnacks = "3";
+// document.write("<br>" + amountSnacksText+amountSnacks + "</br>")
+// var snacksPara = "You will need ";
+// var snacksPara1= " to last you until the ripe old age of ";
+// document.write(snacksPara+(amountSnacks*(estimated-ageCurrent))+snacksPara1+estimated);
+
+// Chapter No #05 Complete;
+
+// Chapter No #06 Start;
+// Question No #01;
+// var a1 = 10;
+// document.write(++a1);
+
+// var a2 = 11;
+// document.write("<br>" + a2++  + "</br>");
+
+// var a3 = 12;
+// document.write(--a3);
+
+// var a4 = 11 ;
+// document.write("<br>" + a4-- + "</br>");
+
+// Question No #02;
+// var a = 2
+// var b = 1
+// var result = --a - --b + ++b + b--;
+// console.log(result)
+
+// Question No #03;
+// var msg = "Enter your name";
+// var userName = prompt(msg,"");
+// console.log(userName)
+
+// Question No #05
+// I can't understand this question;
+
+// Question No #06;
+// var userMarksheetHeading = "Subjects TotalMarks ObtainedMarks Percentage";
+// document.write("<h2>" + userMarksheetHeading + "</h2>");
+
+// var english = "Engli "
+// var marksTotal = 100;
+// var marksObtained =  54;
+// var percentageTotal = " 54%";
+// document.write("<h1>"+english+marksTotal + " " +marksObtained + " " +percentageTotal +  "<h1>");
+
+// var urdu = "Urdu ";
+// var marksTotal1 = 100 ;
+// var marksObtained1 =  54 ;
+// var percentageTotal1 = " 54%";
+// document.write("<h1>" + urdu+marksTotal1+ " " +marksObtained1+percentageTotal1 + " " + "<h1>");
+
+// var math = "Math ";
+// var marksTotal2 = 100;
+// var marksObtained2 =  48 ;
+// var percentageTotal2 = " 48%";
+// document.write("<h1>" + math+marksTotal2+ " " +marksObtained2 + " " +percentageTotal2 + "<h1>");
+
+// var totalResult = "Total ";
+// document.write(totalResult+(marksTotal+marksTotal1+marksTotal2) +"  " );
+// document.write( marksObtained+marksObtained1+marksObtained2 + " ");
+
+// var totalMarksUser = "300";
+// var obtainedMarksUser ="156";
+// document.write(obtainedMarksUser/totalMarksUser*100);
+
+// Chapter NO #06 Complete;
